7850adf252a459571f4b9c4604cd242db19c73b4
Thomas Cayne <thomas.cayne@slashand.com>
2019-10-17 13:12:09 -0500

    formatting for app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 /* This file should NOT be modified.
 Use ./core/core.module.ts to provide singleton services.
 Use ./shared/shared.module.ts to declare and export directives, pipes, modules, and components used by other components.
 Otherwise, use a feature module's component.ts file to apply specific presentation and logic for that feature. */
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        CoreModule,
-        HttpClientModule,
-        LayoutModule,
-        PagesModule,
-        AppRoutingModule,
-        SharedModule,
-        BrowserAnimationsModule
-    ],
-    declarations: [
-        AppComponent
-    ],
-    bootstrap: [
-        AppComponent
-    ]
+  imports: [
+    BrowserModule,
+    CoreModule,
+    HttpClientModule,
+    LayoutModule,
+    PagesModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-
-export class AppModule {
-
-}
+export class AppModule {}
