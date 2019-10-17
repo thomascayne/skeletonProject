1c22e93a5850bf527bf2ff50c54c55029f3c42dc
Thomas Cayne <thomas.cayne@slashand.com>
2019-10-17 13:13:09 -0500

    use ternary and jsdocs styling for comment block

--- src/app/core/data/data.service.ts
+++ src/app/core/data/data.service.ts
@@ -1,29 +1,30 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { Observable, throwError, of } from 'rxjs';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class DataService {
-  
-  
   public constructor(private http: HttpClient) {}
 
-
-  private handleError(error: HttpErrorResponse): Observable<any> {
-    let errorMessage: any;
-
-    if (error.error instanceof Error) {
-      // A client-side or network error occurred.  Handle it accordingly.
-      errorMessage = `An error occurred: ${error.error.message}`;
-    } else {
-      /* The backend returned an unsuccessful response code.
-            The response body may contain clues as to what went wrong. */
-      errorMessage = `Backend returned code ${error.status}, body was: ${
-        error.error
-      }`;
-    }
+  /**
+   * @param error HttpErrorResponse
+   * @return Observable<string>
+   */
+  private handleError(error: HttpErrorResponse): Observable<string> {
+    /**
+     * Use ternary when using only one if-else condition to avoid using early exit
+     *
+     * 1. A client-side or network error occurred.  Handle it accordingly.
+     * else
+     * 2. The backend returned an unsuccessful response code.
+     *    The response body may contain clues as to what went wrong.
+     */
+    const errorMessage =
+      error.error instanceof Error
+        ? `An error occurred: ${error.error.message}`
+        : `Backend returned code ${error.status}, body was: ${error.error}`;
 
     console.error(error);
 
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,30 +1,31 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { Observable, throwError, of } from 'rxjs';
 
-
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class UserService {
+  public constructor(private http: HttpClient) {}
 
-	public constructor(private http: HttpClient) { }
+  /**
+   * @param error HttpErrorResponse
+   * @return Observable<string>
+   */
+  private handleError(error: HttpErrorResponse): Observable<string> {
+    /**
+     * Use ternary when using only one if-else condition to avoid using early exit
+     *
+     * 1. A client-side or network error occurred.  Handle it accordingly.
+     * else
+     * 2. The backend returned an unsuccessful response code.
+     *    The response body may contain clues as to what went wrong.
+     */
+    const errorMessage =
+      error.error instanceof Error
+        ? `An error occurred: ${error.error.message}`
+        : `Backend returned code ${error.status}, body was: ${error.error}`;
 
-	private handleError(error: HttpErrorResponse): Observable<any> {
-		let errorMessage: any;
-
-		if (error.error instanceof Error) {
-			// A client-side or network error occurred.  Handle it accordingly.
-			errorMessage = `An error occurred: ${error.error.message}`;
-		} else {
-			/* The backend returned an unsuccessful response code.
-				  The response body may contain clues as to what went wrong. */
-			errorMessage = `Backend returned code ${error.status}, body was: ${
-				error.error
-				}`;
-		}
-
-		return throwError(new Error(errorMessage));
-	}
-
+    return throwError(new Error(errorMessage));
+  }
 }
